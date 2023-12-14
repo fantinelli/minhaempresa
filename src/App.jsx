5285f30ce7524aedc386fc519c79a2063ec34bd7
fantinelli <fernandaapbercelino@gmail.com>
2023-12-14 10:27:23 -0300

    up

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './pages/Login';
+import Painel from './pages/Painel';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+function App(){
+  const autentic = localStorage.getItem("key") === 'token' || false
+
+  return(
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/*' element={<Login/>}/>
+         {autentic?<Route path='/painel' element={<Painel/>}/>:""}
+        </Routes>
+      </BrowserRouter> 
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Editar.jsx
@@ -0,0 +1,39 @@
+
+import Modal from 'react-bootstrap/Modal'
+import { useState, useEffect } from 'react'
+import { CiEdit } from "react-icons/ci";
+import axios from 'axios';
+import Formulario from './formulario';
+
+
+function Editar({ id}) {
+    const [show, setShow] = useState(false)
+    const [funcionario, setFuncionario] = useState([])
+
+    useEffect(()=>{
+        axios.get("https://apiaulas.thiagodev502.repl.co/funcionarios/"+ id)
+        .then((resposta)=>{
+            setFuncionario(resposta.data)
+        }).catch((error)=>{
+            console.log(error)
+        })
+    },[])
+
+    return (
+        <div>
+            <span style={{ cursor: "pointer" }} onClick={() => setShow(true)} className='text-primary'><CiEdit /></span>
+            <Modal show={show} onHide={() => setShow(false)}>
+                <Modal.Header closeButton>
+                    <h2>Editar</h2>
+                </Modal.Header>
+                <Modal.Body>
+                    <Formulario funcionario={funcionario} setShow={setShow} />
+                </Modal.Body>
+                <Modal.Footer>
+                </Modal.Footer>
+            </Modal>
+        </div>
+    )
+
+}
+export default Editar
